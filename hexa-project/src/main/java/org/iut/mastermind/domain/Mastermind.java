3ad91df3bc9df7a343552004cfa94dd87e37db8f
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2024-10-09 17:59:57 +0200

    push project

---
+++ hexa-project/src/main/java/org/iut/mastermind/domain/Mastermind.java
@@ -0,0 +1,64 @@
+package org.iut.mastermind.domain;
+
+import org.iut.mastermind.domain.partie.Joueur;
+import org.iut.mastermind.domain.partie.Partie;
+import org.iut.mastermind.domain.partie.PartieRepository;
+import org.iut.mastermind.domain.partie.ResultatPartie;
+import org.iut.mastermind.domain.proposition.Reponse;
+import org.iut.mastermind.domain.tirage.MotsRepository;
+import org.iut.mastermind.domain.tirage.ServiceNombreAleatoire;
+import org.iut.mastermind.domain.tirage.ServiceTirageMot;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class Mastermind {
+    private final PartieRepository partieRepository;
+    private final ServiceTirageMot serviceTirageMot;
+
+    public Mastermind(PartieRepository pr, MotsRepository mr, ServiceNombreAleatoire na) {
+        this.partieRepository = pr;
+        this.serviceTirageMot = new ServiceTirageMot(mr, na);
+    }
+
+    // on récupère éventuellement la partie enregistrée pour le joueur
+    // si il y a une partie en cours, on renvoie false (pas de nouvelle partie)
+    // sinon on utilise le service de tirage aléatoire pour obtenir un mot
+    // et on initialise une nouvelle partie et on la stocke
+    public boolean nouvellePartie(Joueur joueur) {
+        if (isJeuEnCours(partieRepository.getPartieEnregistree(joueur))) {
+            return false;
+        }
+        String motATrouvee = serviceTirageMot.tirageMotAleatoire();
+        Partie nouvellePartie = Partie.create(joueur, motATrouvee);
+        partieRepository.create(nouvellePartie);
+        return true;
+    }
+
+    // on récupère éventuellement la partie enregistrée pour le joueur
+    // si la partie n'est pas une partie en cours, on renvoie une erreur
+    // sinon on retourne le resultat du mot proposé
+    public ResultatPartie evaluation(Joueur joueur, String motPropose) {
+        if (isJeuEnCours(partieRepository.getPartieEnregistree(joueur))) {
+            ResultatPartie res = calculeResultat(partieRepository.getPartieEnregistree(joueur).orElse(null), motPropose);
+            return res;
+        } else {
+            return ResultatPartie.ERROR;
+        }
+    }
+
+    // on évalue le résultat du mot proposé pour le tour de jeu
+    // on met à jour la bd pour la partie
+    // on retourne le résulat de la partie
+    private ResultatPartie calculeResultat(Partie partie, String motPropose) {
+        ResultatPartie resPartie = ResultatPartie.create(partie.tourDeJeu(motPropose), partie.isTerminee());
+        partieRepository.update(partie);
+        return resPartie;
+    }
+
+    // si la partie en cours est vide, on renvoie false
+    // sinon, on évalue si la partie est terminée
+    private boolean isJeuEnCours(Optional<Partie> partieEnCours) {
+        return partieEnCours.isPresent();
+    }
+}
---
+++ hexa-project/src/main/java/org/iut/mastermind/domain/proposition/Reponse.java
@@ -0,0 +1,78 @@
+package org.iut.mastermind.domain.proposition;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.unmodifiableList;
+
+public class Reponse {
+    private final String motSecret;
+    private final List<Lettre> resultat = new ArrayList<>();
+    private int position;
+
+    public Reponse(String mot) {
+        this.motSecret = mot;
+    }
+
+    // on récupère la lettre à la position dans le résultat
+    public Lettre lettre(int position) {
+        return resultat.get(position);
+    }
+
+    // on construit le résultat en analysant chaque lettre
+    // du mot proposé
+    public void compare(String essai) {
+        position = 0;
+        for (char c : essai.toCharArray()) {
+            resultat.add(evaluationCaractere(c));
+            position++;
+        }
+    }
+
+    // si toutes les lettres sont placées
+    public boolean lettresToutesPlacees() {
+        boolean toutesPlacees = true;
+        for (Lettre l : resultat) {
+            if (!l.equals(Lettre.PLACEE)) {
+                toutesPlacees = false;
+                break;
+            }
+        }
+        return toutesPlacees;
+    }
+
+    public List<Lettre> lettresResultat() {
+        return unmodifiableList(resultat);
+    }
+
+    // renvoie le statut du caractère
+    private Lettre evaluationCaractere(char carCourant) {
+        if (estPresent(carCourant)) {
+            if (estPlace(carCourant)) {
+                return Lettre.PLACEE;
+            } else {
+                return Lettre.NON_PLACEE;
+            }
+        } else {
+            return Lettre.INCORRECTE;
+        }
+    }
+
+    // le caractère est présent dans le mot secret
+    private boolean estPresent(char carCourant) {
+        boolean estPresent = false;
+        for (char c : motSecret.toCharArray()) {
+            if (carCourant == c) {
+                estPresent = true;
+                break;
+            }
+        }
+        return estPresent;
+    }
+
+    // le caractère est placé dans le mot secret
+    private boolean estPlace(char carCourant) {
+        return carCourant == motSecret.charAt(position);
+    }
+}
